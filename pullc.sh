40b1a3bbc06560cde85b5bb2a287c2e05e6d3df4
satyanani40 <satya.nani.40@gmail.com>
2015-04-19 15:23:00 +0530

    comment

--- pullc.sh
+++ pullc.sh
@@ -1,4 +1,8 @@
 git add -A
 git commit -m 'testing'
 git pull origin  master
+for file in static/app/scripts/controllers/*; do
+java -jar /usr/share/yui-compressor/yui-compressor.jar --type js -v
+static/app/scripts/controllers/${file##*/};  -o static/app/scripts/controllers/${file##*/};
+done
 service weber restart
