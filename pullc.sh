2bcd159fc88db955d610ca03c8603cc377f5bfa0
satya <satya.nani.40@gmail.com>
2015-04-20 20:20:19 +0530

    comment

--- pullc.sh
+++ pullc.sh
@@ -2,25 +2,12 @@ git add -A
 git commit -m 'testing'
 git pull origin  master
 
-for file in static/app/scripts/controllers/*; do
-echo '-------compressing------------'
-echo ${file##*/};
-java -jar /usr/share/yui-compressor/yui-compressor.jar --type js -v
-static/app/scripts/controllers/${file##*/};  -o static/app/scripts/controllers/${file##*/};
-done
-
-for file in static/app/scripts/directives/*; do
-echo '-------compressing------------'
-echo ${file##*/};
-java -jar /usr/share/yui-compressor/yui-compressor.jar --type js -v
-static/app/scripts/directives/${file##*/};  -o static/app/scripts/directives/${file##*/};
-done
 
 echo '-------compressing------------'
-java -jar /usr/share/yui-compressor/yui-compressor.jar --type js -v
-static/app/scripts/services/weberservice.js  -o static/app/scripts/services/weberservice.min.js;
-done
+java -jar /usr/share/yui-compressor/yui-compressor.jar --type js -v static/app/scripts/controllers/allcontrollers.js  -o static/app/scripts/controllers/allcontrollers.min.js
 
 
+echo '-------compressing------------'
+java -jar /usr/share/yui-compressor/yui-compressor.jar --type js -v static/app/scripts/services/allservices.js  -o static/app/scripts/services/allservices.min.js
 
 service weber restart
